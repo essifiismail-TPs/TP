545b40708629cf4470835d538830dd40db264556
essifiismail-TPs <essifiismail@gmail.com>
2026-04-24 22:12:25 +0100

    Add files via upload

---
+++ TP5/ex1.js
@@ -0,0 +1,7 @@
+let nbr1 = Number(prompt("Entrer le 1er nombre:"));
+let nbr2 = Number(prompt("Entrer le 2eme nombre:"));
+
+console.log("somme:" + (nbr1 + nbr2));
+console.log("Difference:" + (nbr1 - nbr2));
+console.log("Produit:" + (nbr1 * nbr2));
+console.log("Quotient:" + (nbr1 / nbr2));
